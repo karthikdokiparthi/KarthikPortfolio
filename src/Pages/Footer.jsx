b8d0ded653d825c04479af3d347161a992a5d7b4
karthikdokiparthik <dokiparthikarthik@gmail.com>
2025-03-06 18:23:02 +0530

    Third commit

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -15,12 +15,12 @@ function Footer() {
                 <div className="footer-middle">
                     <h3>Quick Links</h3>
                     <ul>
-                        <li><a href="#home">Home</a></li>
+                        <li><a href="/">Home</a></li>
                         <li><a href="#about">About</a></li>
                         <li><a href="#skills">Skills</a></li>
                         <li><a href="#projects">Projects</a></li>
                         <li><a href='#profile'>Experience</a></li>
-                        <li><a href="#contact">Contact</a></li>
+                        <li><a href="#contactpage">Contact</a></li>
                     </ul>
                 </div>
 
